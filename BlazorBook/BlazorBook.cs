7421fee3f6b9d37d38536f795ac831e27f03aa24
Allan Eagle <allan@allaneagle.com>
2023-03-03 22:02:46 +0000

    Added basic grouping

--- BlazorBook/BlazorBook.cs
+++ BlazorBook/BlazorBook.cs
@@ -13,9 +13,18 @@ namespace BlazorBook
     {
     }
 
+    public class ComponentGroup
+    {
+        public string Name { get; set; }
+        public List<ComponentMetadata> Components { get; set; } = new();
+        public List<ComponentGroup> Children { get; set; } = new();
+    }
+
     public static class Stories
     {
         static Dictionary<string, ComponentMetadata> _components = new();
+
+        public static ComponentGroup Root { get; private set; } = new() { Name = "Root" };
 
         public static IEnumerable<ComponentMetadata> GetAll() => _components.Values;
 
@@ -25,15 +34,37 @@ namespace BlazorBook
         {
             var slug = t.FullName.Replace(".", "_").ToLower();
             var displayName = t.GetCustomAttributes(typeof(DisplayNameAttribute)).Cast<DisplayNameAttribute>().FirstOrDefault();
-            _components.Add(
-                slug, 
-                new ComponentMetadata
+
+            var path = (displayName?.DisplayName ?? slug).Split("/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            var metadata = new ComponentMetadata
+            {
+                Slug = slug,
+                Name = path.Last(),
+                Type = t
+            };
+            _components.Add(slug, metadata);
+
+            void fileComponent(ComponentGroup group, string[] path, ComponentMetadata componentMetadata)
+            {
+                if (path.Length == 1)
                 {
-                    Slug = slug,
-                    Name = displayName?.DisplayName ?? slug,
-                    Type = t
+                    group.Components.Add(componentMetadata);
                 }
-            );
+                else
+                {
+                    var childGroup = group.Children.FirstOrDefault(x => x.Name == path[0]);
+                    if (childGroup == null)
+                    {
+                        childGroup = new ComponentGroup { Name = path[0] };
+                        group.Children.Add(childGroup);
+                    }
+
+                    fileComponent(childGroup, path[1..], componentMetadata);
+                }
+            }
+
+            fileComponent(Root, path, metadata);
         }
 
         public static void RegisterAllStories(this WebAssemblyHostBuilder host, params Assembly[] assemblies)
