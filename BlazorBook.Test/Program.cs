3f5a657450b250e01a919511cb2f2d9e0a5b6d19
Allan Eagle <allan@allaneagle.com>
2023-03-03 21:38:18 +0000

    Changes

--- BlazorBook.Test/Program.cs
+++ BlazorBook.Test/Program.cs
@@ -5,10 +5,10 @@ using System.Reflection;
 using BlazorBook;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.RegisterAllStories(Assembly.Load("BlazorBook.Test.Stories"));
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RegisterAllStories(Assembly.Load("BlazorBook.Test.Stories"));
 
 await builder.Build().RunAsync();
